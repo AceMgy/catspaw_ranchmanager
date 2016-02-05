a96a9ca10b989aed324c4e531b114814dc4487ee
AceMgy <AceMgy@gmail.com>
2016-02-05 13:23:19 -0700

    Initial models and migrations for Owner app.

---
+++ catspaw/Owner/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Phone(models.Model):
+    """ Contains a single phone number and what type it is. """
+    categories = ((1, "home"), (2, "work"), (3, "fax"), (4, "cell"), (5, "other"))
+    phone_cat = models.IntegerField(choices=categories)
+    phone_num = models.PositiveIntegerField(unique=True)
+
+class Address(models.Model):
+    """ Contains information on a single address """
+    street = models.CharField(max_length=50)
+    street2 = models.CharField(max_length=50, null=True, blank=True)
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=2)
+    zip_code = models.PositiveIntegerField()
+
+class Owner(models.Model):
+    """ Contains information on an owner, be it an individual or company."""
+    phones = models.ManyToManyField(Phone)
+    address = models.ForeignKey(Address, null=True, blank=True)
+    email = models.CharField(max_length=50, null=True, blank=True)
+
+class Person(Owner):
+    """ Contains specific information on an individual."""
+    first_name = models.CharField(max_length=50)
+    middle_name = models.CharField(max_length=50, null=True, blank=True)
+    last_name = models.CharField(max_length=50)
+
+class Company(Owner):
+    """ Contains specific information on a company. """
+    name = models.CharField(max_length=50, unique=True)
+    website = models.CharField(max_length=50, null=True, blank=True)
+    people = models.ManyToManyField(Person)
